bd7f39e08f91428430eeffdd42a022511435376b
max <max@draminski.com>
2022-05-26 11:13:22 +0200

    example app

---
+++ app1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include <xyz.h>
+
+int main(int, char**) {
+    say_hello();
+    std::cout << "Hello, world!\n";
+}
---
+++ app2/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <xyz.h>
+
+int main(int, char**) {
+    say_hello();
+    std::cout << "Hello, world!\n";
+}
